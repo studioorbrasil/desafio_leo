d6959ca3c7ef6bc647740403c65e733a499a841d
studioorbrasil <studioorbrasil@gmail.com>
2022-02-20 00:50:48 -0300

    Classe CRUD
    Início da classe CRUD

---
+++ classes/CRUD.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class CRUD {
+    public $servidor = SERVIDOR;
+    public $usuario = NOMEUSERBD;
+    public $senha = USERPASS;
+    public $banco = NOMEBD;
+    public $connecta = NULL;
+    private $tabela;
+    public $linhas = -1;
+
+    public function getTabela(){
+        return $this->tabela;
+    }
+
+    public function setTabela($tabela){
+        $this ->tabela = $tabela;
+    }
+
+    public function __construct(){
+        $this->conexao();
+    }
+    public function conexao(){
+        $this->connecta= NEW PDO('mysql:host=localhost;dbname=desafio', NOMEUSERBD, USERPASS,  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $this->connecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 require 'funcoes.php';
 
+$modulo = isset($_GET['m']) && $_GET['m'] != "" ? $_GET['m'] : "login";
+$tela = isset($_GET['t']) && $_GET['t'] != "" ? $_GET['t'] : "login";
+
+loadModulo($modulo,$tela);
 ?>
