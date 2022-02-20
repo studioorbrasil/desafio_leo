38648743f5ad3ce92d44dd60b274e167eecda3ad
studioorbrasil <studioorbrasil@gmail.com>
2022-02-20 00:51:44 -0300

    Update CRUD.class.php
    conexão PDO

--- classes/CRUD.class.php
+++ classes/CRUD.class.php
@@ -24,3 +24,8 @@ class CRUD {
         $this->connecta= NEW PDO('mysql:host=localhost;dbname=desafio', NOMEUSERBD, USERPASS,  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $this->connecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    }
+
+}
+
+?>
